288f7fac81548700820349aed614ee43a2aade06
tombola <tombola@github>
2026-02-23 14:50:42 +0000

    Release v1.0.5

--- src/batch_labels/standalone.py
+++ src/batch_labels/standalone.py
@@ -9,7 +9,7 @@ import urllib.request
 import uvicorn
 
 # Keep in sync with pyproject.toml [project] version
-APP_VERSION = "1.0.4"
+APP_VERSION = "1.0.5"
 GITHUB_REPO = "vitalseeds/batch_labels"
 
 
